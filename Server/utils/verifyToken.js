398122493b69afa37c56a9eb97653c03418460d9
Huỳnh Ngọc Tài <128598991+TaiHuynhPL@users.noreply.github.com>
2023-08-03 15:51:49 +0700

    Update verifyToken.js

--- Server/utils/verifyToken.js
+++ Server/utils/verifyToken.js
@@ -9,7 +9,7 @@ const verifyToken = (req, res, next) => {
     return next(createError(401, "You are not authenticated!"));
   }
 
-  jwt.verify(token, process.env.JWT, (err, user) => {
+  jwt.verify(token, "qwertyuiop", (err, user) => {
     if (err) return next(createError(403, "Token is not valid!"));
     req.user = user;
     next();
